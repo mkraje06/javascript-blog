350293b445b62043645f8653051346f6f9808edc
Maksymilian Krajewski <maksymilian.krajwski@yahoo.com>
2021-07-19 09:10:01 +0200

    change code JS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 'use strict';
 
+
 const titleClickHandler = function(event){
     console.log('Link was clicked!');
+    event.preventDefault();
   
     /* remove class 'active' from all article links  */
-  
+const activeLinks = document.querySelectorAll(class="active");
+
+    for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+}
+
     /* add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+    const clickedElement = this;
+    
+}
+
   
     /* remove class 'active' from all articles */
   
