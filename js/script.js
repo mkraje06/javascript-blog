6a99a0ad4e0c3dc97ea9db686ea2f3531402ed1c
Maksymilian Krajewski <maksymilian.krajwski@yahoo.com>
2021-07-21 10:22:12 +0200

    push repo

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const titleClickHandler = function(event){
     event.preventDefault();
   
     /* remove class 'active' from all article links  */
-const activeLinks = document.querySelectorAll(class="active");
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
@@ -14,17 +14,24 @@ const activeLinks = document.querySelectorAll(class="active");
 
     /* add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement);
-    const clickedElement = this;
-    
+    clickedElement.classList.add('active');
 }
 
   
     /* remove class 'active' from all articles */
-  
+    const activeArticles = document.querySelectorAll('article.post');
+    for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+    
     /* get 'href' attribute from the clicked link */
-  
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
+    
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+    }
+    
     /* add class 'active' to the correct article */
   }
   
