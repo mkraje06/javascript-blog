fd5ae09b52f20ac5671d54cb226b3cf064b4230d
Maksymilian Krajewski <maksymilian.krajewski@yahoo.com>
2021-07-24 12:10:26 +0200

    bug code

--- js/script.js
+++ js/script.js
@@ -49,4 +49,45 @@ const titleClickHandler = function(event){
   optTagsListSelector = '.tags.list';
 
   function generateTitleLinks(customSelector = ''){
+
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+   function generateTitleLinks(){ 
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector + customSelector);
+  let html = '';
+  for (let article of articles){  
+
+  /* get the article id */
+  const articleId = article.getAttribute('id');
+
+  /* find the title element */
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
   
+  /* get the title from the title element */
+
+
+  /* create HTML of the link */
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+  /* insert link into titleList */
+  html = html + linkHTML;
+      
+}  
+  titleList.innerHTML = html;
+
+}
+
+generateTitleLinks();
+
+const links = document.querySelectorAll('.titles a');
+for(let link of links){
+link.addEventListener('click', titleClickHandler);
+}
